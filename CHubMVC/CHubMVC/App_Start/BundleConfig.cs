f9f14f61d0eba95b5de5fd2d6f1438d3209cab1c
on609 <jhy5188@126.com>
2017-01-10 14:37:20 +0800

    validation init

--- CHubMVC/CHubMVC/App_Start/BundleConfig.cs
+++ CHubMVC/CHubMVC/App_Start/BundleConfig.cs
@@ -20,8 +20,8 @@ namespace CHubMVC
             bundles.Add(new ScriptBundle("~/bundles/progressbar").Include(
                         "~/Scripts/bootstrap-progressbar.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/boostrapvalidate").Include(
+                        "~/Scripts/bootstrap3-validation.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
