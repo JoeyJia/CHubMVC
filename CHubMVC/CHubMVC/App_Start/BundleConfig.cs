e436c4d37d9aff503f0790acc7f1e97d5d3d5721
on609 <jhy5188@126.com>
2017-01-10 15:41:41 +0800

    add validation input to small

--- CHubMVC/CHubMVC/App_Start/BundleConfig.cs
+++ CHubMVC/CHubMVC/App_Start/BundleConfig.cs
@@ -20,8 +20,10 @@ namespace CHubMVC
             bundles.Add(new ScriptBundle("~/bundles/progressbar").Include(
                         "~/Scripts/bootstrap-progressbar.js"));
 
+            //bundles.Add(new ScriptBundle("~/bundles/boostrapvalidate").Include(
+            //            "~/Scripts/bootstrap3-validation.js"));
             bundles.Add(new ScriptBundle("~/bundles/boostrapvalidate").Include(
-                        "~/Scripts/bootstrap3-validation.js"));
+            "~/Scripts/validator.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
