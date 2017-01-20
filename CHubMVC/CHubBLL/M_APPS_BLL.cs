f608da5bb791fae67684b5969f16f7ebe0cc0a40
oo450 <jhy5188@126.com>
2017-01-20 15:36:17 +0800

    add new page orderList.cshtml

---
+++ CHubMVC/CHubBLL/M_APPS_BLL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDAL;
+using CHubDBEntity;
+
+namespace CHubBLL
+{
+    public class M_APPS_BLL
+    {
+        private readonly M_APPS_DAL dal;
+
+        public M_APPS_BLL()
+        {
+            dal = new M_APPS_DAL();
+        }
+        public M_APPS_BLL(CHubEntities db)
+        {
+            dal = new M_APPS_DAL(db);
+        }
+
+        public List<M_APPS> GetMAppList()
+        {
+            return dal.GetMAppsList();
+        }
+
+    }
+}
---
+++ CHubMVC/CHubDAL/M_APPS_DAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDBEntity;
+
+namespace CHubDAL
+{
+    public class M_APPS_DAL : BaseDAL
+    {
+        public M_APPS_DAL()
+            : base() { }
+
+        public M_APPS_DAL(CHubEntities db)
+            : base(db) { }
+
+        public List<M_APPS> GetMAppsList()
+        {
+            //add condition for active ind later
+            return db.M_APPS.OrderBy(a=>a.CREATION_DATE).ToList();
+        }
+
+
+    }
+}
