ca8deeaf92b27791f9c6b955a5c8502a1933da9c
oo450 <jhy5188@126.com>
2017-03-03 16:10:50 +0800

    Add ITT index page template

---
+++ CHubMVC/CHubMVC/Controllers/ITTController.cs
@@ -0,0 +1,23 @@
+﻿using CHubBLL;
+using CHubCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CHubMVC.Controllers
+{
+    public class ITTController : BaseController
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            
+            //APP_RECENT_PAGES_BLL rpBLL = new APP_RECENT_PAGES_BLL();
+            //rpBLL.Add(appUser, CHubEnum.PageNameEnum.tcmnt.ToString(), this.Request.Url.AbsoluteUri);
+
+            return View();
+        }
+    }
+}
