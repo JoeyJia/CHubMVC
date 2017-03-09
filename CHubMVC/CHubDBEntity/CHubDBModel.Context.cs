c1418186f2a2c44f41d82b996fe2c27829bf45ad
oo450 <jhy5188@126.com>
2017-03-09 14:10:46 +0800

    Add m_calendar table

--- CHubMVC/CHubDBEntity/CHubDBModel.Context.cs
+++ CHubMVC/CHubDBEntity/CHubDBModel.Context.cs
@@ -86,5 +86,6 @@ namespace CHubDBEntity
         public virtual DbSet<ITT_SHIPPING_D> ITT_SHIPPING_D { get; set; }
         public virtual DbSet<ITT_TRAN_LOAD> ITT_TRAN_LOAD { get; set; }
         public virtual DbSet<V_ITT_SHIPPING_SMRY> V_ITT_SHIPPING_SMRY { get; set; }
+        public virtual DbSet<M_CALENDAR> M_CALENDAR { get; set; }
     }
 }
