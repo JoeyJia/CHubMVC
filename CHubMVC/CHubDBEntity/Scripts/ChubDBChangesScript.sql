5eb0f191621df27bb6b4b5b07b688bfe6a615385
oo450 <jhy5188@126.com>
2017-03-21 10:44:52 +0800

    db changes scripts

---
+++ CHubMVC/CHubDBEntity/Scripts/ChubDBChangesScript.sql
@@ -0,0 +1,6 @@
+﻿----DB Chagnes Script
+----From 3-21-17
+
+--1.For text chagnes Will Bill to Way Bill
+update app_pages  set display='-> Way Bill Data Entry' , description ='--- Way Bill Information Load (Import Part Tracking)'
+where page_name='wbentry';
