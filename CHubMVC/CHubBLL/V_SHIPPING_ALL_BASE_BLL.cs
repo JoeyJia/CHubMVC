3ff69ba6b999248fdefdbae0a5f82d36cc948e16
oo450 <jhy5188@126.com>
2017-03-24 14:06:27 +0800

    add bll and DAL

---
+++ CHubMVC/CHubBLL/V_SHIPPING_ALL_BASE_BLL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDAL;
+using CHubDBEntity;
+
+namespace CHubBLL
+{
+    public class V_SHIPPING_ALL_BASE_BLL
+    {
+        private readonly V_SHIPPING_ALL_BASE_DAL dal;
+
+        public V_SHIPPING_ALL_BASE_BLL()
+        {
+            dal = new V_SHIPPING_ALL_BASE_DAL();
+        }
+        public V_SHIPPING_ALL_BASE_BLL(CHubEntities db)
+        {
+            dal = new V_SHIPPING_ALL_BASE_DAL(db);
+        }
+
+        public V_SHIPPING_ALL_BASE GetFirstBaseInfo(string wayBillNo)
+        {
+            return dal.GetFirstBaseInfo(wayBillNo);
+        }
+
+
+    }
+}
---
+++ CHubMVC/CHubDAL/V_SHIPPING_ALL_BASE_DAL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDBEntity;
+
+namespace CHubDAL
+{
+    public class V_SHIPPING_ALL_BASE_DAL : BaseDAL
+    {
+        public V_SHIPPING_ALL_BASE_DAL()
+            : base() { }
+
+        public V_SHIPPING_ALL_BASE_DAL(CHubEntities db)
+            : base(db) { }
+
+        public V_SHIPPING_ALL_BASE GetFirstBaseInfo(string wayBillNo)
+        {
+            return db.V_SHIPPING_ALL_BASE.FirstOrDefault(a => a.TRACKING_NO == wayBillNo);
+        }
+
+
+    }
+}
