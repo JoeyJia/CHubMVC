78e0f55d244f44bafed0eaaf9c58c3a29377bdc3
oo450 <jhy5188@126.com>
2017-03-30 10:11:15 +0800

    add entity unmanaged view model

---
+++ CHubMVC/CHubDBEntity/UnmanagedModel/V_INV_PDC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHubDBEntity.UnmanagedModel
+{
+    public class V_INV_PDC
+    {
+        public string SYS_ID { get; set; }
+        public string PART_NO { get; set; }
+        public string WAREHOUSE { get; set; }
+        public string WH_ALIAS { get; set; }
+        public string BAY_NO { get; set; }
+        public decimal QTY_AVL { get; set; }
+        public decimal QTY_ONHAND { get; set; }
+        public decimal QTY_RESERVED { get; set; }
+        public decimal QTY_IN_RECEIVING { get; set; }
+        public decimal QTY_IN_QA { get; set; }
+        public DateTime? LOAD_DATE { get; set; }
+    }
+}
---
+++ CHubMVC/CHubDBEntity/UnmanagedModel/V_OPEN_QTY_SO_PDC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHubDBEntity.UnmanagedModel
+{
+    public class V_OPEN_QTY_SO_PDC
+    {
+        public string PART_NO { get; set; }
+        public string WAREHOUSE { get; set; }
+        public string WH_ALIAS { get; set; }
+        public string QTY_BACKORDERED { get; set; }
+        public string QTY_OPENING { get; set; }
+        public string QTY_RESERVED { get; set; }
+        public string QTY_IN_PICKING { get; set; }
+    }
+}
