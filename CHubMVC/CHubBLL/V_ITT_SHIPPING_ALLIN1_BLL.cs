061583e089ffef78820bb5d48b302377ef844dff
oo450 <jhy5188@126.com>
2017-04-05 15:06:36 +0800

    Add DAL and BLL class

---
+++ CHubMVC/CHubBLL/V_ITT_SHIPPING_ALLIN1_BLL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDAL;
+using CHubDBEntity;
+
+namespace CHubBLL
+{
+    public class V_ITT_SHIPPING_ALLIN1_BLL
+    {
+        private readonly V_ITT_SHIPPING_ALLIN1_DAL dal;
+
+        public V_ITT_SHIPPING_ALLIN1_BLL()
+        {
+            dal = new V_ITT_SHIPPING_ALLIN1_DAL();
+        }
+        public V_ITT_SHIPPING_ALLIN1_BLL(CHubEntities db)
+        {
+            dal = new V_ITT_SHIPPING_ALLIN1_DAL(db);
+        }
+
+    }
+}
---
+++ CHubMVC/CHubDAL/V_ITT_SHIPPING_ALLIN1_DAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDBEntity;
+
+namespace CHubDAL
+{
+    public class V_ITT_SHIPPING_ALLIN1_DAL : BaseDAL
+    {
+        public V_ITT_SHIPPING_ALLIN1_DAL()
+            : base() { }
+
+        public V_ITT_SHIPPING_ALLIN1_DAL(CHubEntities db)
+            : base(db) { }
+
+    }
+}
