a4b2171f43e94e05282fec146cdf59e868cb0780
oo450 <jhy5188@126.com>
2017-04-05 14:07:00 +0800

    update sql script

--- CHubMVC/CHubDBEntity/Scripts/ChubDBChangesScript.sql
+++ CHubMVC/CHubDBEntity/Scripts/ChubDBChangesScript.sql
@@ -4,3 +4,6 @@
 --1.For text chagnes Will Bill to Way Bill
 update app_pages  set display='-> Way Bill Data Entry' , description ='--- Way Bill Information Load (Import Part Tracking)'
 where page_name='wbentry';
+
+--2.For easyquery page update sql
+update APP_PAGES set URL='/itt/easyquery' where PAGE_NAME='ittezinq';
