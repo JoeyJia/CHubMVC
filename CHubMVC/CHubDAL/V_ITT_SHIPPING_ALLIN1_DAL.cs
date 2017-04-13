1548063cfa659effb76daab1220a2931a4e1c2b3
oo450 <jhy5188@126.com>
2017-04-13 16:13:47 +0800

    shipment some changes

--- CHubMVC/CHubDAL/V_ITT_SHIPPING_ALLIN1_DAL.cs
+++ CHubMVC/CHubDAL/V_ITT_SHIPPING_ALLIN1_DAL.cs
@@ -17,7 +17,7 @@ namespace CHubDAL
 
         public List<V_ITT_SHIPPING_ALLIN1> GetShipmentData(string partNo)
         {
-            return db.V_ITT_SHIPPING_ALLIN1.Where(a => a.PART_NO == partNo).ToList();
+            return db.V_ITT_SHIPPING_ALLIN1.Where(a => a.PART_NO == partNo).OrderByDescending(a=>a.SHIP_DATE).ToList();
         }
 
     }
