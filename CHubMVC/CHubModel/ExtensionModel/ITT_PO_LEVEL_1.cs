a2a6860d589530af8bc0d0911539a5f24c985885
oo450 <jhy5188@126.com>
2017-04-17 15:25:31 +0800

    po mid1

---
+++ CHubMVC/CHubModel/ExtensionModel/ITT_PO_LEVEL_1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHubModel.ExtensionModel
+{
+    public class ITT_PO_LEVEL_1
+    {
+        public string BUY_FOR { get; set; }
+
+        public string PUR_ORDER_ID { get; set; }
+
+        public string WAREHOUSE { get; set; }
+
+        public string BUY_FROM_COMPANY { get; set; }
+
+        public string COMPANY_NAME { get; set; }
+
+        public string EC_PUR_ORDER_TYPE_CODE { get; set; }
+
+        public string BUYER_CODE { get; set; }
+    }
+}
---
+++ CHubMVC/CHubModel/ExtensionModel/ITT_PO_LEVEL_2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHubModel.ExtensionModel
+{
+    class ITT_PO_LEVEL_2
+    {
+        public decimal PUR_ORDER_LINE_NO { get; set; }
+
+        public string PART_NO { get; set; }
+
+        public string PUR_ORDER_DESC { get; set; }
+
+        public string UOM_CODE { get; set; }
+    }
+}
