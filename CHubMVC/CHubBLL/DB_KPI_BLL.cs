dfbce8911502f464ca5826ec77c78a4630e8e6ae
oo450 <jhy5188@126.com>
2017-04-26 14:56:46 +0800

    Add  KPI DAL and BLL

---
+++ CHubMVC/CHubBLL/DB_KPI_BLL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDAL;
+using CHubDBEntity;
+
+namespace CHubBLL
+{
+    public class DB_KPI_BLL
+    {
+        private readonly DB_KPI_DAL dal;
+
+        public DB_KPI_BLL()
+        {
+            dal = new DB_KPI_DAL();
+        }
+        public DB_KPI_BLL(CHubEntities db)
+        {
+            dal = new DB_KPI_DAL(db);
+        }
+
+    }
+}
---
+++ CHubMVC/CHubDAL/DB_KPI_HISTORY_DAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDBEntity;
+
+namespace CHubDAL
+{
+    public class DB_KPI_HISTORY_DAL : BaseDAL
+    {
+        public DB_KPI_HISTORY_DAL()
+            : base() { }
+
+        public DB_KPI_HISTORY_DAL(CHubEntities db)
+            : base(db) { }
+
+
+    }
+}
