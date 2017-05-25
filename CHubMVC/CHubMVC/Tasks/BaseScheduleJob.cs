d07ba1f79f7ea9b0ec16751f0dd2d4b56747ab15
oo450 <jhy5188@126.com>
2017-05-25 17:25:35 +0800

    Add baseScheduleJob

---
+++ CHubMVC/CHubMVC/Tasks/BaseScheduleJob.cs
@@ -0,0 +1,60 @@
+﻿using Hangfire;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CHubMVC.Tasks
+{
+    public class BaseScheduleJob
+    {
+        public string ScheduleID { get; set; }
+        public List<string> MsgIDList = new List<string>();
+
+        public virtual void Execute()
+        {
+            GetMsgList();
+
+            foreach (var item in MsgIDList)
+            {
+                try
+                {
+                    if (IsExecuted(item))
+                        continue;
+                    SendEmail(item);
+                    LogStatus(item);
+                }
+                catch (Exception ex)
+                {
+                    //Fail
+                    LogStatus(item);
+                    string msg = ex.Message;
+                    //continue;
+                }
+            }
+
+        }
+
+        public bool IsExecuted(string msgID)
+        {
+            return false;
+        }
+
+        public bool SendEmail(string msgID)
+        {
+
+            return true;
+        }
+
+        public void LogStatus(string msgID)
+        {
+
+        }
+
+        public void GetMsgList()
+        {
+            MsgIDList = new List<string>();
+        }
+
+    }
+}
