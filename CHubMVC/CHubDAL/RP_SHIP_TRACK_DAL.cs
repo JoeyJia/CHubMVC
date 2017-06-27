b50ac4e75936366c5732430a765b0ebf6dfac681
oo450 <jhy5188@126.com>
2017-06-27 13:51:05 +0800

    add BLL and DAL class

---
+++ CHubMVC/CHubDAL/RP_SHIP_TRACK_DAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDBEntity;
+
+namespace CHubDAL
+{
+    public class RP_SHIP_TRACK_DAL : BaseDAL
+    {
+        public RP_SHIP_TRACK_DAL()
+            : base() { }
+
+        public RP_SHIP_TRACK_DAL(CHubEntities db)
+            : base(db) { }
+
+
+    }
+}
