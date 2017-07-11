68a4188b58553ac6ee67b0a438223a85913ff3b2
oo450 <jhy5188@126.com>
2017-07-11 10:05:44 +0800

    Add some basic for autoPrint

---
+++ CHubMVC/CHubCommon/Printer/Externs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHubCommon.Printer
+{
+    public class Externs
+    {
+        [DllImport("winspool.drv")]
+        public static extern bool SetDefaultPrinter(string Name);
+    }
+}
---
+++ CHubMVC/CHubCommon/Printer/PrintHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace CHubCommon.Printer
+{
+    public class PrintHelper
+    {
+
+
+        static public void PrintFile(string filePath,string printerName = null)
+        {
+            Process pro = new Process();
+            pro.StartInfo.FileName = filePath;
+            pro.StartInfo.CreateNoWindow = true;
+            pro.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            pro.StartInfo.Verb = "Print";
+            if (!string.IsNullOrEmpty(printerName))
+            {
+                Externs.SetDefaultPrinter(printerName);
+            }
+
+            pro.Start();
+
+        }
+    }
+}
