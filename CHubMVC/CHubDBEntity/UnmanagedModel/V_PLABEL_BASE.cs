8771cb8e1f0869738dfeb6a34126a4ddfefc6494
oo450 <jhy5188@126.com>
2017-07-20 11:44:05 +0800

    add two unmanaged entity class

---
+++ CHubMVC/CHubDBEntity/UnmanagedModel/V_PLABEL_BASE.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHubDBEntity.UnmanagedModel
+{
+    public class V_PLABEL_BASE
+    {
+        public string PART_CATALOG { get; set; }
+        public string PART_NO { get; set; }
+        public string DESCRIPTION { get; set; }
+        public string DESC_CN { get; set; }
+        public string SHORT_DESCRIPTION { get; set; }
+        public string COUNTRY_OF_ORIGIN { get; set; }
+        public string UNIT_MEAS { get; set; }
+        public string PART_STATUS { get; set; }
+        public decimal? PART_HEIGHT { get; set; }
+        public decimal? PART_LENGTH { get; set; }
+        public decimal? PART_WIDTH { get; set; }
+        public decimal? PART_WEIGHT { get; set; }
+        public string CURRENT_SALES_STATUS_CODE { get; set; }
+        public string PRINT_PART_NO { get; set; }
+        public decimal? MIN_ORDER_QTY { get; set; }
+        public decimal? QTY_IN_CARTON { get; set; }
+        public DateTime? CREATE_DATE { get; set; }
+        public DateTime? RECORD_DATE { get; set; }
+        public string NOTE_TEXT { get; set; }
+
+    }
+}
---
+++ CHubMVC/CHubDBEntity/UnmanagedModel/V_SHIPMENT_D_ALL1ONE.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHubDBEntity.UnmanagedModel
+{
+    public class V_SHIPMENT_D_ALL1ONE
+    {
+        public string SHIP_ID { get; set; }
+        public string WH_ID { get; set; }
+        public string HOST_EXT_ID { get; set; }
+        public string SHPSTS { get; set; }
+        public string CARCOD { get; set; }
+        public string CARNAM { get; set; }
+        public string TRACK_NUM { get; set; }
+        public DateTime? ADDDTE { get; set; }
+        public DateTime? ALCDTE { get; set; }
+        public DateTime? STGDTE { get; set; }
+        public DateTime? LODDTE { get; set; }
+        public DateTime? ENTDTE { get; set; }
+        public DateTime? MODDTE { get; set; }
+        public string MOD_USR_ID { get; set; }
+        public string ADRNAM { get; set; }
+        public string ADRLN1 { get; set; }
+        public string ADRLN2 { get; set; }
+        public string ADRLN3 { get; set; }
+        public string ADRCTY { get; set; }
+        public string ADRSTC { get; set; }
+        public string ADRPSZ { get; set; }
+        public string CTRY_NAME { get; set; }
+        public string PHNNUM { get; set; }
+        public string EMAIL_ADR { get; set; }
+        public string FIRST_NAME { get; set; }
+        public string LAST_NAME { get; set; }
+        public string CONT_NAME { get; set; }
+        public string SHIP_LINE_ID { get; set; }
+        public string ORDNUM { get; set; }
+        public string ORDLIN { get; set; }
+        public string ORDSLN { get; set; }
+        public decimal? PCKQTY { get; set; }
+        public decimal? INPQTY { get; set; }
+        public decimal? STGQTY { get; set; }
+        public decimal? SHPQTY { get; set; }
+        public string PRTNUM { get; set; }
+        public decimal? UNTQTY { get; set; }
+        public string LODNUM { get; set; }
+        public string BTCUST { get; set; }
+        public string STCUST { get; set; }
+        public string ORDTYP { get; set; }
+        public string SALES_ORDNUM { get; set; }
+        public string CSTPRT { get; set; }
+        public string VC_CPONUM { get; set; }
+        public string VC_CPOLIN { get; set; }
+        public string STOLOC { get; set; }
+        public decimal? VC_PALWGT { get; set; }
+        public decimal? VC_PALLEN { get; set; }
+        public decimal? VC_PALWID { get; set; }
+        public decimal? VC_PALHGT { get; set; }
+
+    }
+}
