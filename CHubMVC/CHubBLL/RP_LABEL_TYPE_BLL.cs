7369485dfb41659c54c7072315bb2291afdb049b
oo450 <jhy5188@126.com>
2017-07-21 14:05:38 +0800

    Add BLL and DAL

---
+++ CHubMVC/CHubBLL/RP_LABEL_TYPE_BLL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDAL;
+using CHubDBEntity;
+
+namespace CHubBLL
+{
+    public class RP_LABEL_TYPE_BLL
+    {
+        private readonly RP_LABEL_TYPE_DAL dal;
+
+        public RP_LABEL_TYPE_BLL()
+        {
+            dal = new RP_LABEL_TYPE_DAL();
+        }
+        public RP_LABEL_TYPE_BLL(CHubEntities db)
+        {
+            dal = new RP_LABEL_TYPE_DAL(db);
+        }
+
+
+    }
+}
---
+++ CHubMVC/CHubDAL/RP_LABEL_TYPE_DAL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDBEntity;
+
+namespace CHubDAL
+{
+    public class RP_LABEL_TYPE_DAL : BaseDAL
+    {
+        public RP_LABEL_TYPE_DAL()
+            : base() { }
+
+        public RP_LABEL_TYPE_DAL(CHubEntities db)
+            : base(db) { }
+
+
+
+    }
+}
