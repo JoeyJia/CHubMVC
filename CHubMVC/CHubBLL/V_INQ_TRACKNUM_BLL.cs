65f905da47d88f824783239ab34b6883442f16a2
oo450 <jhy5188@126.com>
2017-08-03 14:29:19 +0800

    update  DAL and BLL and unmanaged model for  track num query

---
+++ CHubMVC/CHubBLL/V_INQ_TRACKNUM_BLL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDAL;
+using CHubDBEntity;
+
+namespace CHubBLL
+{
+    public class V_INQ_TRACKNUM_BLL
+    {
+        private readonly V_INQ_TRACKNUM_DAL dal;
+
+        public V_INQ_TRACKNUM_BLL()
+        {
+            dal = new V_INQ_TRACKNUM_DAL();
+        }
+        public V_INQ_TRACKNUM_BLL(CHubEntities db)
+        {
+            dal = new V_INQ_TRACKNUM_DAL(db);
+        }
+
+    }
+}
---
+++ CHubMVC/CHubDAL/V_INQ_TRACKNUM_DAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDBEntity;
+
+namespace CHubDAL
+{
+    public class V_INQ_TRACKNUM_DAL : BaseDAL
+    {
+        public V_INQ_TRACKNUM_DAL()
+            : base() { }
+
+        public V_INQ_TRACKNUM_DAL(CHubEntities db)
+            : base(db) { }
+
+
+    }
+}
