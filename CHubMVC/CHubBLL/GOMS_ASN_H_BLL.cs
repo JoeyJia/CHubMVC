0b0f6ed38e2aced297f610d6b4fda56a2ba8a0eb
oo450 <jhy5188@126.com>
2017-08-23 15:09:06 +0800

    Add DAL and BLL for GMMS_ASN_H

---
+++ CHubMVC/CHubBLL/GOMS_ASN_H_BLL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDAL;
+using CHubDBEntity;
+
+namespace CHubBLL
+{
+    public class GOMS_ASN_H_BLL
+    {
+        private readonly GOMS_ASN_H_DAL dal;
+
+        public GOMS_ASN_H_BLL()
+        {
+            dal = new GOMS_ASN_H_DAL();
+        }
+        public GOMS_ASN_H_BLL(CHubEntities db)
+        {
+            dal = new GOMS_ASN_H_DAL(db);
+        }
+
+    }
+}
---
+++ CHubMVC/CHubDAL/GOMS_ASN_H_DAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDBEntity;
+
+namespace CHubDAL
+{
+    public class GOMS_ASN_H_DAL : BaseDAL
+    {
+        public GOMS_ASN_H_DAL()
+            : base() { }
+
+        public GOMS_ASN_H_DAL(CHubEntities db)
+            : base(db) { }
+
+
+    }
+}
