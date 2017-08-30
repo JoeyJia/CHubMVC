3c19cdc6cd9fd6a575523bf6a663c5a272ef3f81
oo450 <jhy5188@126.com>
2017-08-30 10:38:25 +0800

    batch job setup

--- CHubMVC/BatchJob/Startup.cs
+++ CHubMVC/BatchJob/Startup.cs
@@ -4,6 +4,8 @@ using Owin;
 using System;
 using Hangfire.SQLite;
 using CHubBLL;
+using CHubBLL.OtherProcess;
+using System.Configuration;
 
 [assembly: OwinStartup(typeof(BatchJob.Startup))]
 
@@ -27,13 +29,19 @@ namespace BatchJob
 
             // Add scheduled jobs
             //Jobs jobs = new Jobs();
-            BatchJobs jobs = new BatchJobs();
+            //BatchJobs jobs = new BatchJobs();
             ////    jobs.SendM1Mail();
             //RecurringJob.AddOrUpdate(() => Console.WriteLine("{0} Background job completed successfully!", DateTime.Now.ToString()), Cron.Minutely);
 
+            string interval = ConfigurationManager.AppSettings["PackCronMin"];
+            int min = int.Parse(interval);
+            JobsManager jobs = new JobsManager();
+            RecurringJob.AddOrUpdate(() => jobs.PackPrint(), Cron.MinuteInterval(min));
+
+
             //RecurringJob.AddOrUpdate(() => Console.WriteLine("{0} Background job completed successfully!", DateTime.Now.ToString()), Cron.Monthly(3, 2));
             //RecurringJob.AddOrUpdate(() => jobs.SendMail(1), Cron.Minutely);
-            RecurringJob.AddOrUpdate(() => jobs.SendM1Mail("M1",null,null), Cron.Daily(20));
+            //RecurringJob.AddOrUpdate(() => jobs.SendM1Mail("M1",null,null), Cron.Daily(20));
             // Map Dashboard to the `http://<your-app>/hangfire` URL.
             ////app.UseHangfireDashboard();
             ////app.UseHangfireServer();
