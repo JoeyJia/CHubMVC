dbc64090fe64345595ddf4f3353eae6538719986
oo450 <jhy5188@126.com>
2017-11-28 14:52:04 +0800

    CtrAdd 20171128

---
+++ CHubMVC/CHubMVC/Controllers/IAController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CHubMVC.Controllers
+{
+    public class IAController : BaseController
+    {
+        [Authorize]
+        public ActionResult IACode()
+        {
+            return View();
+        }
+
+    }
+}
