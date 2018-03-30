3be2ada040b77236297208f16108e819ddffc23e
oo450 <jhy5188@126.com>
2018-03-30 10:05:40 +0800

    20180330 Update

--- CHubMVC/CHubDBEntity/TC_PART_CATEGORY.cs
+++ CHubMVC/CHubDBEntity/TC_PART_CATEGORY.cs
@@ -12,7 +12,7 @@ namespace CHubDBEntity
     using System;
     using System.Collections.Generic;
     using Newtonsoft.Json;
-    
+
     public partial class TC_PART_CATEGORY
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
