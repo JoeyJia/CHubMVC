9565bb17bf9591e35789c96ccd781dc1577e8d08
oo450 <jhy5188@126.com>
2018-04-10 14:22:32 +0800

    20180410 Update

---
+++ CHubMVC/CHubDBEntity/UnmanagedModel/JD_ADDR_CONVERT.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHubDBEntity.UnmanagedModel
+{
+    public class JD_ADDR_CONVERT
+    {
+        public decimal JID { get; set; }
+        public string GOMS_ADDR { get; set; }
+        public string CONVERTED_ADDR { get; set; }
+        public DateTime? CREATE_DATE { get; set; }
+        public string UPDATED_BY { get; set; }
+        public DateTime? RECORD_DATE { get; set; }
+    }
+}
