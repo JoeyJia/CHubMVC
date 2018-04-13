e569263e545c1ab7a6e7c8c0b5bc2868bd0a170d
oo450 <jhy5188@126.com>
2018-04-13 10:28:57 +0800

    20180413 Update

---
+++ CHubMVC/CHubBLL/V_XCEC_ORDER_LN_BASE_BLL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDBEntity;
+using CHubDBEntity.UnmanagedModel;
+using CHubDAL;
+
+namespace CHubBLL
+{
+    public class V_XCEC_ORDER_LN_BASE_BLL
+    {
+        private V_XCEC_ORDER_LN_BASE_DAL dal;
+
+        public V_XCEC_ORDER_LN_BASE_BLL()
+        {
+            dal = new V_XCEC_ORDER_LN_BASE_DAL();
+        }
+
+        public V_XCEC_ORDER_LN_BASE_BLL(CHubEntities db)
+        {
+            dal = new V_XCEC_ORDER_LN_BASE_DAL(db);
+        }
+    }
+}
