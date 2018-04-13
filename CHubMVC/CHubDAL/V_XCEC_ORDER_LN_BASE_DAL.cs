79b82e5c8379bd1d38b9b711f60ae5798c6241d4
oo450 <jhy5188@126.com>
2018-04-13 10:29:25 +0800

    20180413 Update

---
+++ CHubMVC/CHubDAL/V_XCEC_ORDER_LN_BASE_DAL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDBEntity;
+using CHubDBEntity.UnmanagedModel;
+
+namespace CHubDAL
+{
+    public class V_XCEC_ORDER_LN_BASE_DAL : BaseDAL
+    {
+        public V_XCEC_ORDER_LN_BASE_DAL() : base()
+        {
+
+        }
+        public V_XCEC_ORDER_LN_BASE_DAL(CHubEntities db) : base(db)
+        {
+
+        }
+    }
+}
