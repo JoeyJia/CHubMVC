a26bbf142f5355a0a55e8cded8006e1dcdd1c19a
oo450 <jhy5188@126.com>
2018-04-17 11:33:53 +0800

    20180417 Update

---
+++ CHubMVC/CHubDBEntity/UnmanagedModel/V_XCEC_ADDR_ALL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHubDBEntity.UnmanagedModel
+{
+    public class V_XCEC_ADDR_ALL
+    {
+        public string ALIAS_NAME { get; set; }
+        public string SYSID { get; set; }
+        public string WAREHOUSE { get; set; }
+        public string CUSTOMER_NO { get; set; }
+        public int? BILL_TO_LOCATION { get; set; }
+        public decimal? SHIP_TO_LOCATION { get; set; }
+        public decimal? DEST_LOCATION { get; set; }
+        public string CONTACT { get; set; }
+        public string TEL { get; set; }
+        public string ADDR_NAME { get; set; }
+        public string ADDR_1 { get; set; }
+        public string ADDR_2 { get; set; }
+        public string ADDR_3 { get; set; }
+
+
+
+
+    }
+}
