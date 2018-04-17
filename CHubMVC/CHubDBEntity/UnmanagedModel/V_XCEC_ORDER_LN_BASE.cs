df3e6bb3f23fe7865a70081a22f294b9e4e3065e
oo450 <jhy5188@126.com>
2018-04-17 11:34:07 +0800

    20180417 Update

---
+++ CHubMVC/CHubDBEntity/UnmanagedModel/V_XCEC_ORDER_LN_BASE.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHubDBEntity.UnmanagedModel
+{
+    public class V_XCEC_ORDER_LN_BASE
+    {
+        public string CUST_ORDER_NO { get; set; }
+        public string CUST_PART_NO { get; set; }
+        public string DESCRIPTION { get; set; }
+        public string DESC_CN { get; set; }
+        public string KITS_FLAG { get; set; }
+        public decimal ORDER_LINE_NO { get; set; }
+        public string PART_NO { get; set; }
+        public decimal? QTY { get; set; }
+    }
+}
