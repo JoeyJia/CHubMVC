75c465b2a0bcbcc8909772556e9bca03f4558801
oo450 <jhy5188@126.com>
2018-04-24 15:18:25 +0800

    20180424 Add

---
+++ CHubMVC/CHubMVC/Controllers/MDController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CHubCommon;
+using CHubDBEntity;
+using CHubDBEntity.UnmanagedModel;
+using CHubModel;
+using CHubBLL;
+
+namespace CHubMVC.Controllers
+{
+    public class MDController : BaseController
+    {
+    }
+}
