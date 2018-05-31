51d9f6b79d75777e84c66085445f29b8f758d09e
oo450 <jhy5188@126.com>
2018-05-31 15:44:20 +0800

    20180531 Add

---
+++ CHubMVC/CHubMVC/Controllers/EXPController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CHubDBEntity.UnmanagedModel;
+using CHubBLL;
+using System.Web.Mvc;
+using CHubCommon;
+
+namespace CHubMVC.Controllers
+{
+    public class EXPController : BaseController
+    {
+        public ActionResult EXPWB()
+        {
+            string appUser = Session[CHubConstValues.SessionUser].ToString();
+            APP_RECENT_PAGES_BLL rpBLL = new APP_RECENT_PAGES_BLL();
+            rpBLL.Add(appUser, CHubEnum.PageNameEnum.expwb.ToString(), this.Request.Url.AbsoluteUri);
+            return View();
+        }
+    }
+}
