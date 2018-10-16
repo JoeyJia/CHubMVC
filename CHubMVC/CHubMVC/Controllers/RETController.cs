b1580e70e7123e6905617bdf919b717662c36354
oo450 <jhy5188@126.com>
2018-10-16 13:29:39 +0800

    20181016 Add

---
+++ CHubMVC/CHubMVC/Controllers/RETController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CHubCommon;
+using CHubDBEntity.UnmanagedModel;
+using CHubModel;
+using CHubBLL;
+using System.Text;
+using System.Data;
+using System.IO;
+
+namespace CHubMVC.Controllers
+{
+    public class RETController : BaseController
+    {
+        public ActionResult RetInv()
+        {
+            string appUser = Session[CHubConstValues.SessionUser].ToString();
+            APP_RECENT_PAGES_BLL rpBLL = new APP_RECENT_PAGES_BLL();
+            rpBLL.Add(appUser, CHubEnum.PageNameEnum.retinv.ToString(), this.Request.Url.AbsoluteUri);
+            ViewBag.AppUser = appUser;
+            return View();
+        }
+
+
+
+    }
+}
