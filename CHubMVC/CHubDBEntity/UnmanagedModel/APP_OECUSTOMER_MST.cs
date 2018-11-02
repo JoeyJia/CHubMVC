6ee457307b252b032d36998d8027787261319d14
oo450 <jhy5188@126.com>
2018-11-02 08:58:56 +0800

    20181102 Update

---
+++ CHubMVC/CHubDBEntity/UnmanagedModel/APP_OECUSTOMER_MST.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHubDBEntity.UnmanagedModel
+{
+    public class APP_OECUSTOMER_MST
+    {
+        public string CUSTOMER_NO { get; set; }
+        public string CUST_NAME { get; set; }
+        public DateTime CREATE_DATE { get; set; }
+        public string CWS_FLAG { get; set; }
+        public string FLAG01 { get; set; }
+        public string FLAG02 { get; set; }
+        public string FLAG03 { get; set; }
+        public string NOTE { get; set; }
+        public DateTime LOAD_DATE { get; set; }
+    }
+}
