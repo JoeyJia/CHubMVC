78da7b67bd7ddb3f39330b83e1279274a434a3e5
oo450 <jhy5188@126.com>
2018-11-02 08:58:35 +0800

    20181102 Update

---
+++ CHubMVC/CHubBLL/CWSCUST_BLL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDBEntity.UnmanagedModel;
+using CHubDAL;
+
+namespace CHubBLL
+{
+    public class CWSCUST_BLL
+    {
+        private CWSCUST_DAL dal;
+        public CWSCUST_BLL()
+        {
+            dal = new CWSCUST_DAL();
+        }
+        public List<APP_OECUSTOMER_MST> CwsCustSearch(string CUSTOMER_NO)
+        {
+            return dal.CwsCustSearch(CUSTOMER_NO);
+        }
+        public void CwsCustSave(APP_OECUSTOMER_MST arg)
+        {
+            dal.CwsCustSave(arg);
+        }
+    }
+}
