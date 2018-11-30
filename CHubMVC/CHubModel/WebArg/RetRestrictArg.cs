5f86db60bedc1b85b1c215b0bb3254c9790bf66f
oo450 <jhy5188@126.com>
2018-11-30 14:16:47 +0800

    20181130 Update

---
+++ CHubMVC/CHubModel/WebArg/RetRestrictArg.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHubModel.WebArg
+{
+    public class RetRestrictArg
+    {
+        public string PART_NO { get; set; }
+        public string RETURN_RESTRICT { get; set; }
+        public string RETURN_MOQ { get; set; }
+    }
+}
