42bcbd9e5db27932beb5d58616c8b7e35b957cdd
RQ365 <RQ365@Commins.com>
2019-12-09 17:43:09 +0800

    Add resultcode Bll

---
+++ CHubMVC/CHubBLL/XX_ASNDIFF_RESULT_CODE_BLL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDAL;
+using CHubModel.WebArg;
+using CHubDBEntity.UnmanagedModel;
+using System.Data;
+
+namespace CHubBLL
+{
+    public class XX_ASNDIFF_RESULT_CODE_BLL
+    {
+        private XX_ASNDIFF_RESULT_CODE_DAL dal;
+        public XX_ASNDIFF_RESULT_CODE_BLL()
+        {
+            dal = new XX_ASNDIFF_RESULT_CODE_DAL();
+        }
+
+        public List<XX_ASNDIFF_RESULT_CODE> GetXXASNDIFFRESULTCODEList()
+        {
+            return dal.GetASNDIFFRESULTCODEList();
+        }
+    }
+}
