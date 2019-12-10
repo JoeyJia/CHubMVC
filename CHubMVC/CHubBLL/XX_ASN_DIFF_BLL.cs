07cad92b3bd2fee8fb45ddeece9eb27afc773bfc
RQ365 <RQ365@Commins.com>
2019-12-10 11:42:51 +0800

    添加asndiff Bll

---
+++ CHubMVC/CHubBLL/XX_ASN_DIFF_BLL.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CHubDAL;
+using CHubModel.WebArg;
+using CHubDBEntity.UnmanagedModel;
+using System.Data;
+
+namespace CHubBLL
+{
+    public class XX_ASN_DIFF_BLL
+    {
+        private XX_ASN_DIFF_DAL dal;
+        public XX_ASN_DIFF_BLL()
+        {
+            dal = new XX_ASN_DIFF_DAL();
+        }
+
+        /// <summary>
+        /// 根据条件查询差异订单
+        /// </summary>
+        /// <param name="AsnDiffSearch"></param>
+        /// <returns></returns>
+        public List<XX_ASN_DIFF> GetAsnDiffListBySearch(AsnDiffArg AsnDiffSearch)
+        {
+            return dal.GetAsnDiffListBySearch(AsnDiffSearch);
+        }
+
+        /// <summary>
+        /// 更新订单备注 备注累加
+        /// </summary>
+        /// <param name="asnid"></param>
+        /// <param name="remark"></param>
+        /// <returns></returns>
+        public bool UpdateXXAsnDiffRemark(string asnid, string remark)
+        {
+            return dal.UpdateXXAsnDiffRemark(asnid, remark);
+        }
+        /// <summary>
+        /// 更新差异数据 空为成功 否则返回失败原因
+        /// </summary>
+        /// <param name="asndiff"></param>
+        /// <returns></returns>
+        public string UpdateXXAsnDiff(XX_ASN_DIFF asndiff)
+        {
+            return dal.UpdateXXAsnDiff(asndiff);
+        }
+
+        /// <summary>
+        /// 根据asn单号获取差异数据
+        /// </summary>
+        /// <param name="warehouse"></param>
+        /// <param name="AsnID"></param>
+        /// <returns></returns>
+        public List<V_DIS_ASN_BASE> GetAsnDiffInfo(string warehouse, string AsnID)
+        {
+            return dal.GetAsnDiffInfo(warehouse, AsnID);
+        }
+        public void SaveAsnDiff(string warehouse, string asnid, out string SaveMsg)
+        {
+            dal.SaveAsnDiff(warehouse, asnid, out SaveMsg);
+        }
+    }
+}
--- CHubMVC/CHubDAL/XX_ASN_DIFF_DAL.cs
+++ CHubMVC/CHubDAL/XX_ASN_DIFF_DAL.cs
@@ -18,6 +18,108 @@ namespace CHubDAL
             ccHelper = new CHubCommonHelper();
         }
 
+        /// <summary>
+        /// 更新订单明细
+        /// </summary>
+        /// <param name="asndiff"></param>
+        /// <returns></returns>
+        public string UpdateXXAsnDiff(XX_ASN_DIFF asndiff)
+        {
+            string sqlwhere = string.Empty;
+            if (asndiff == null || asndiff.ASN_DIFF_ID == 0)
+            {
+                return "请求数据有误";
+            }
+            string sqlmodel = string.Format(@"select * from XX_ASN_DIFF where ASN_DIFF_ID=" + asndiff.ASN_DIFF_ID);
+            List<XX_ASN_DIFF> res = ccHelper.ExecuteSqlToList<XX_ASN_DIFF>(sqlmodel);
+            if (res == null || res.Count <= 0)
+            {
+                return "订单不存在";
+            }
+            if (res[0].IS_CLOSE == "1")
+            {
+                return "订单已关闭";
+            }
+            if (!string.IsNullOrEmpty(asndiff.DISP_ACTION))
+            {
+                sqlwhere += " DISP_ACTION='" + asndiff.DISP_ACTION + "' ,";
+            }
+            if (!string.IsNullOrEmpty(asndiff.PLANNER_NOTES))
+            {
+                sqlwhere += " PLANNER_NOTES='" + asndiff.PLANNER_NOTES + "' ,";
+            }
+            if (!string.IsNullOrEmpty(asndiff.WRITE_OFF_NO))
+            {
+                sqlwhere += " WRITE_OFF_NO='" + asndiff.WRITE_OFF_NO + "' ,";
+            }
+            if (!string.IsNullOrEmpty(asndiff.INVOICE_NO))
+            {
+                sqlwhere += " INVOICE_NO='" + asndiff.INVOICE_NO + "' ,";
+            }
+            if (asndiff.SHIP_DATE != null && asndiff.SHIP_DATE.Value != null)
+            {
+                sqlwhere += " SHIP_DATE='" + asndiff.SHIP_DATE.Value.ToString() + "' ,";
+            }
+            if (asndiff.QTY_FIN_WRITEOFF != 0)
+            {
+                sqlwhere += " QTY_FIN_WRITEOFF=" + asndiff.QTY_FIN_WRITEOFF + " ,";
+            }
+            if (asndiff.CLAIM_DATE != null && asndiff.CLAIM_DATE.Value != null)
+            {
+                sqlwhere += " CLAIM_DATE='" + asndiff.CLAIM_DATE.Value.ToString() + "' ,";
+            }
+            if (!string.IsNullOrEmpty(asndiff.CLAIM_NOTES))
+            {
+                sqlwhere += " CLAIM_NOTES='" + asndiff.CLAIM_NOTES + "' ,";
+            }
+            if (!string.IsNullOrEmpty(asndiff.CLAIM_NO))
+            {
+                sqlwhere += " CLAIM_NO='" + asndiff.CLAIM_NO + "' ,";
+            }
+            if (!string.IsNullOrEmpty(asndiff.CLAIM_RESULT))
+            {
+                sqlwhere += " CLAIM_RESULT='" + asndiff.CLAIM_RESULT + "' ,";
+            }
+            if (!string.IsNullOrEmpty(asndiff.CREDIT_NO))
+            {
+                sqlwhere += " CREDIT_NO='" + asndiff.CREDIT_NO + "' ,";
+            }
+
+            if (!string.IsNullOrEmpty(asndiff.CLAIM_DENY_REASON))
+            {
+                sqlwhere += " CLAIM_DENY_REASON='" + asndiff.CLAIM_DENY_REASON + "' ,";
+            }
+            if (!string.IsNullOrEmpty(asndiff.RECEIVE_CREDIT))
+            {
+                sqlwhere += " RECEIVE_CREDIT='" + asndiff.RECEIVE_CREDIT + "' ,";
+            }
+            if (asndiff.CLOSE_DATE != null && asndiff.CLOSE_DATE.Value != null)
+            {
+                sqlwhere += " CLOSE_DATE='" + asndiff.CLOSE_DATE + "' ,";
+                sqlwhere += " IsClose='1' ,";
+            }
+            if (!string.IsNullOrEmpty(sqlwhere))
+            {
+                sqlwhere = sqlwhere.Substring(0, sqlwhere.Length - 1);
+                string sql = string.Format(@"UPDATE XX_ASN_DIFF SET " + sqlwhere + " where ASN_DIFF_ID= " + asndiff.ASN_DIFF_ID);
+                ccHelper.ExecuteNonQuery(sql);
+            }
+            return "";
+        }
+
+        /// <summary>
+        /// 更新订单备注
+        /// </summary>
+        /// <param name="asnid"></param>
+        /// <param name="remark"></param>
+        /// <returns></returns>
+        public bool UpdateXXAsnDiffRemark(string asnid, string remark)
+        {
+            string sql = string.Format(@"UPDATE XX_ASN_DIFF SET DIFFREMARK+=' " + remark + " where ASN_DIFF_ID= " + asnid);
+            ccHelper.ExecuteNonQuery(sql);
+            return true;
+        }
+
         public List<XX_ASN_DIFF> GetAsnDiffListBySearch(AsnDiffArg AsnDiffSearch)
         {
             string sqlwhere = "";
@@ -55,12 +157,12 @@ namespace CHubDAL
             var result = ccHelper.ExecuteSqlToList<V_DIS_ASN_BASE>(sql);
             return result;
         }
-        public void MP_MAINConfirm(string warehouse, string asnid,out string SaveMsg)
+        public void SaveAsnDiff(string warehouse, string asnid,out string SaveMsg)
         {
             OracleParameter[] param = new OracleParameter[] {
-                new OracleParameter(":V_SO_NO",OracleDbType.Varchar2),
-                new OracleParameter(":V_opt_code",OracleDbType.Varchar2),
-                new OracleParameter(":V_Reason",OracleDbType.Varchar2)
+                new OracleParameter(":asn_wh",OracleDbType.Varchar2),
+                new OracleParameter(":asn_manid",OracleDbType.Varchar2),
+                new OracleParameter(":asnmsg",OracleDbType.Varchar2)
             };
             param[0].Value = warehouse;
             param[1].Value = asnid;
